595c9f17918dd7562eb9c4ad767cd4ccadf4b3b0
Charles Marino <charlesmarino1@gmail.com>
2017-11-09 18:26:10 -0500

    update

--- example.py
+++ example.py
@@ -62,49 +62,8 @@ def run_bot():
     # DON'T USE MODE 5 FOR A LONG PERIOD. YOU RISK YOUR ACCOUNT FROM GETTING BANNED
     ## USE MODE 5 IN BURST MODE, USE IT TO UNFOLLOW PEOPLE AS MANY AS YOU WANT IN SHORT TIME PERIOD
 
-    mode = 0 
-
-    #print("You choose mode : %i" %(mode))
-    #print("CTRL + C to cancel this operation or wait 30 seconds to start")
-    #time.sleep(30)
-
-    if mode == 0:
+  
         bot.auto_mod()
-
-    elif mode == 1:
-        check_status(bot)
-        while bot.self_following - bot.self_follower > 200:
-            unfollow_protocol(bot)
-            time.sleep(10 * 60)
-            check_status(bot)
-        while bot.self_following - bot.self_follower < 400:
-            while len(bot.user_info_list) < 50:
-                feed_scanner(bot)
-                time.sleep(5 * 60)
-                follow_protocol(bot)
-                time.sleep(10 * 60)
-                check_status(bot)
-
-    elif mode == 2:
-        bot.bot_mode = 1
-        bot.new_auto_mod()
-
-    elif mode == 3:
-        unfollow_protocol(bot)
-        time.sleep(10 * 60)
-
-    elif mode == 4:
-        feed_scanner(bot)
-        time.sleep(60)
-        follow_protocol(bot)
-        time.sleep(10 * 60)
-
-    elif mode == 5:
-        bot.bot_mode = 2
-        unfollow_protocol(bot)
-
-    else:
-        print("Wrong mode!")
 
 
 if __name__ == '__main__':
